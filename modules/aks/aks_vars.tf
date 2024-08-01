631838dbb377387bf4bfca557091cc3bf702c364
ADawath8063 <aishwarya8063@gmail.com>
2024-08-01 10:49:54 +0530

    Terraform Day4 - Project 5.2

---
+++ modules/aks/aks_vars.tf
@@ -0,0 +1,9 @@
+variable "location" {
+    description =  "(Required) Keyvault Location"
+    default = "centralindia"  
+}
+
+variable "rg_name" {
+    description =  "Name of the Resource Group"
+    default = "techslate-ade-rg806"  
+}
